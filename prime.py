577ca53d3c77d04fd3a6dd9b0e3d498dc39bd88e
pathisahithi107 <168920021+pathisahithi107@users.noreply.github.com>
2024-06-12 16:17:16 +0530

    Add files via upload

---
+++ prime.py
@@ -0,0 +1,10 @@
+print("Hello World");
+n=int(input("enter a number"));
+for i in range(2,n):
+  if n>1:
+    print("it is not a prime number");
+  elif(n%i==0):
+    print("it is not a prime number");
+  else:
+    print("It is not a prime number"); 
+print(i)
